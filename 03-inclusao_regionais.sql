c17b6dfb38f6f693de70960d1eec18d1b534b0bc
Aline Regina Carlos <aline.carlos@gmail.com>
2013-06-19 17:18:47 -0300

    Create 03-inclusao_regionais.sql

---
+++ 03-inclusao_regionais.sql
@@ -0,0 +1,26 @@
+insert into "servicos".regional 
+values ( 1, 'Região Funcional 1', 'Vale do Caí, Paranhama - Encosta da Serra, Vale do Rio dos Sinos, Metropolitano - Delta do Jacuí, Centro-Sul', true);
+
+insert into "servicos".regional 
+values ( 2, 'Região Funcional 2', 'Vale do Taquari e Vale do Rio Pardo', true);
+
+insert into "servicos".regional 
+values ( 3, 'Região Funcional 3', 'Serra, Hortênsias e Campos em Cima da Serra', true);
+
+insert into "servicos".regional 
+values ( 4, 'Região Funcional 4', 'Litoral', true);
+
+insert into "servicos".regional 
+values ( 5, 'Região Funcional 5', 'Sul', true);
+
+insert into "servicos".regional 
+values ( 6, 'Região Funcional 6', 'Fronteira Oeste - Campanha', true);
+
+insert into "servicos".regional 
+values ( 7, 'Região Funcional 7', 'Missões, Fronteira Noroeste, Celeiro e Noroeste Colonial', true);
+
+insert into "servicos".regional 
+values ( 8, 'Região Funcional 8', 'Central, Alto do Jacuí, Vale do Jaguari e Jacuí Centro', true);
+
+insert into "servicos".regional 
+values ( 9, 'Região Funcional 9', 'Alto da Serra do Botucaraí, Médio Alto Uruguai, Nordeste, Norte, Produção e Rio da Várzea', true);
