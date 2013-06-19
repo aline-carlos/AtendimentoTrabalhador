7d72e05bb458b779e96b7b9d9e7ff38ca194b6f6
Aline Regina Carlos <aline.carlos@gmail.com>
2013-06-19 17:21:38 -0300

    Create 06-inclusao_usuario.sql

---
+++ 06-inclusao_usuario.sql
@@ -0,0 +1,5 @@
+insert into "servicos".usuario values('aline-carlos' , 'Aline Regina Carlos', true, '1234');
+
+insert into "servicos".usuario values('marcos-spomberg' , 'Marcos Paes Spomberg', true, '1234');
+
+insert into "servicos".usuario values('migracao' , 'Usuário de migração dos dados', true, '1234');
