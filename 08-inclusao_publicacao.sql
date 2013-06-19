e9fabeff07d41ee350ebe40e3c2fd50f32a20b41
Aline Regina Carlos <aline.carlos@gmail.com>
2013-06-19 17:23:16 -0300

    Create 08-inclusao_publicacao.sql

---
+++ 08-inclusao_publicacao.sql
@@ -0,0 +1,3 @@
+insert into "servicos".publicacao values ( 201301, true, true, true, true, 'migracao', current_timestamp );
+insert into "servicos".publicacao values ( 201302, true, true, true, true, 'migracao', current_timestamp );
+insert into "servicos".publicacao values ( 201303, true, true, true, true, 'migracao', current_timestamp );
