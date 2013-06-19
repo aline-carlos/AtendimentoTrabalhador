914b6a7ae5f215705e6956be682e3c24f9f9eaea
Aline Regina Carlos <aline.carlos@gmail.com>
2013-06-19 17:16:51 -0300

    Create 01-criacao_database.sql

---
+++ 01-criacao_database.sql
@@ -0,0 +1,8 @@
+/* Database creation must be done outside an multicommand file.
+   These commands were put in this file only for convenience.
+*/
+-- object: fgtas | type: DATABASE -- 
+CREATE DATABASE fgtas
+  ENCODING = 'UTF8'
+;
+
